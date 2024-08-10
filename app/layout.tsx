17c0a576ac5c2242740d38aa38552567a383bcce
Anup Mishra <anupmishra@Anups-MacBook-Air.local>
2024-08-11 00:09:58 +0530

    add vercel speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         <div className="mx-auto max-w-2xl px-6">
           {header}
           {children}
+          <SpeedInsights />
           {footer}
         </div>
       </body>
