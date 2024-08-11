1fe2a0f01a91411167edcf0b4fed2209cb4e8f58
Anup Mishra <anupmishra@Anups-MacBook-Air.local>
2024-08-11 19:45:31 +0530

    Updated social media icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +20,55 @@ export default function RootLayout({
   const header = (
     <header>
       <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
-        <Link href="/"><h1 className="text-2xl text-white font-bold mt-4">Study DevOps Blog</h1> </Link>
-        <p className="text-slate-300">Welcome to StudyDevOps blog</p>
+      <Image
+          src="/Anup.jpeg"
+          width={50}
+          height={50}
+          className="mx-auto rounded-full"
+          alt={"logo"}
+        />
+        <Link href="/"><h1 className="text-2xl text-white font-bold mt-4">StudyDevOps</h1> </Link>
+        <p className="text-slate-300">DevOps blog</p>
       </div>
     </header>
   );
   const footer = (
     <footer>
-      <div className="border-t border-slate-500 mt-6 py-6 text-center text-slate-400">
-        <p> Developed by Anup Copyright@2024</p>
+      <div className="border-t border-slate-500 mt-6 py-6 text-slate-400 flex flex-col md:flex-row justify-between items-center">
+        <p className="order-2 md:order-1 mt-8 md:mt-0">&copy; 2024 StudyDevOps. All Rights Reserved.</p>
+        <div className="flex space-x-4 order-1 md:order-2 mt-4 md:mt-0">
+          <Link href="https://x.com/anupdevops" target="_blank">
+            <Image
+              src="/x-icon.png"
+              width={30}
+              height={30}
+              className="rounded-full"
+              alt="Twitter"
+            />
+          </Link>
+          <Link href="https://linkedin.com/in/anupdevops" target="_blank">
+            <Image
+              src="/linkedin-icon.png"
+              width={30}
+              height={30}
+              className="rounded-full"
+              alt="LinkedIn"
+            />
+          </Link>
+          <Link href="https://github.com/AnupDevOps" target="_blank">
+            <Image
+              src="/github-icon.png"
+              width={30}
+              height={30}
+              className="rounded-full"
+              alt="Github"
+            />
+          </Link>
+        </div>
       </div>
     </footer>
   );
+  
   return (
     <html lang="en">
       <body>
